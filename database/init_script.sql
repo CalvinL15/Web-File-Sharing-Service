196711b779c9df1007f037e3b650d7fcf3f50ff9
Calvin Liusnando <calvin.liusnando@staffbase.com>
2022-06-18 14:58:05 +0200

    other stuffs

---
+++ database/init_script.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS files (
+  file_id text PRIMARY KEY, -- sha256 string of the file
+  -- file_uploader UUID,
+  file_name text CHECK (file_name IS NOT NULL AND file_name != ''),
+  file_size integer CHECK (file_size > 0 AND file_size <= 10485760), -- max file size: 10 MiB
+  file_upload_time timestamp DEFAULT CURRENT_TIMESTAMP,
+  last_download_time timestamp DEFAULT CURRENT_TIMESTAMP,
+  file_type text,
+  is_file_blocked boolean
+  -- FOREIGN KEY (file_uploader) REFERENCES users(user_id) 
+);
+
+CREATE TABLE IF NOT EXISTS requests (
+  file_id text,
+  request_reason text CHECK (request_reason IS NOT NULL and request_reason != ''),
+  request_type integer CHECK (request_type = 0 OR request_type = 1), -- 0 = block, 1 = unblock
+  request_status text DEFAULT 'Pending' CHECK (request_status = 'Pending' OR request_status = 'Declined' OR request_status = 'Accepted'),
+  FOREIGN KEY (file_id) REFERENCES files(file_id) 
+);
+
+-- CREATE TABLE IF NOT EXISTS users (
+--   user_id UUID PRIMARY KEY,
+--   username text CHECK (username IS NOT NULL AND length(username) >= 6),
+--   passwrd text NOT NULL,
+--   role number CHECK (role = 0 OR role = 1), -- 0 = normal user, 1 = admin
+--   UNIQUE(username)
+-- )
+
+-- CREATE TABLE IF NOT EXISTS users_files_link (
+--   username text, 
+--   file_id text,
+--   FOREIGN KEY (username) REFERENCES users(username),
+--   FOREIGN KEY (file_id) REFERENCES files(file_id)
+-- )
+
+
--- server/index.js
+++ server/index.js
@@ -1,9 +1,64 @@
-const express = require("express");
+const config = require('./config.json');
+const { username, password } = config;
+
+const express = require('express');
+const cheerio = require('cheerio');
+let request = require('request');
+request = request.defaults({jar: true});
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+// initial authentication process
+request.get('https://www.tu-chemnitz.de/informatik/DVS/blocklist', function (error, response, body) {
+  const wtc_login_url =  response.request._redirect.redirects[0].redirectUri;
+  const login_samlds_auth = decodeURIComponent(wtc_login_url).split("Login?")[1];
+  let href_url = "";
+
+  request.get("https://www.tu-chemnitz.de/Shibboleth.sso/Login?" + login_samlds_auth + decodeURIComponent("&entityID=https%3A%2F%2Fwtc.tu-chemnitz.de%2Fshibboleth"), function(error, response, body) {
+    const get_href_step_one = body.split("href=")[1];
+    href_url = get_href_step_one.split(">here")[0].substring(1).slice(0, -1);
+    request.get(href_url, function(error, response, body) {
+      const login_url = response.request.uri.href;
+      const authState = href_url.split("AuthState=")[1];
+      request.post(login_url, {
+        form: {
+          username: username,
+          AuthState: decodeURIComponent(authState)
+        }}, 
+        function (error, response, body) {
+          const redirect_url = response.body.split("href=")[1].split("\">")[0].substring(1);
+          request.get(redirect_url, function (error, response, body){
+            request.post(response.request._redirect.redirects[0].redirectUri, {
+              form: {
+                username: username,
+                password: password,
+                AuthState: decodeURIComponent(authState)
+              }
+            }, 
+            function (error, response, body) {
+              const $ = cheerio.load(body);
+              $.html();
+              const saml_value = $('input')[1].attribs.value;
+              const relay_state = $('input')[2].attribs.value;
+              request.post("https://www.tu-chemnitz.de/Shibboleth.sso/SAML2/POST", {
+                form: {
+                  SAMLResponse: saml_value,
+                  RelayState: relay_state
+                }
+              }, function (error, response, body) {
+                console.log('Successfully authenticated!');
+              })
+            })
+          }
+        )
+      }
+    );
+  });
+  });
+});
+
